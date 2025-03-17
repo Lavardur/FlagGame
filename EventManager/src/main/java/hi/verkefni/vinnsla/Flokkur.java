8074dcda54e472bc2dcf877ebacd7e0a5dc26a01
Lavardur <antonben44@gmail.com>
2025-03-17 17:28:31 +0000

    flokkar

--- EventManager/src/main/java/hi/verkefni/vinnsla/Flokkur.java
+++ EventManager/src/main/java/hi/verkefni/vinnsla/Flokkur.java
@@ -1,5 +1,27 @@
 package hi.verkefni.vinnsla;
 
-public class Flokkur {
+public enum Flokkur {
+    EXERCISE("🚴"),
+    WORK("📊"),
+    RELAX("🧘"),
+    TV("📺"),
+    READ("📚"),
+    EVENT("🎭"),
+    CODE("💻"),
+    COFFEE("☕️"),
+    EAT("🍽"),
+    SHOP("🛒"),
+    SLEEP("😴");
 
+    private String emoji;
+
+    Flokkur(String emoji)
+    {
+        this.emoji = emoji;
+    }
+
+    public String getEmoji()
+    {
+        return this.emoji;
+    }
 }
